5ada14b340b1cf67333442cebdc7e86b0a3a3651
Purnima Aggarwal <pgwebdev@gmail.com>
2018-11-15 17:28:00 +0000

    changed docker file

--- code/index.php
+++ code/index.php
@@ -1,2 +1,23 @@
-<?php 
-echo phpinfo();
+<?php
+phpinfo();exit;
+$countOfArr = function($arr)
+    {
+		printf("count is %d\r\n", count($arr));
+    };
+
+$countOfArr(array("A", "B", "C"));
+
+
+$message = 2;
+$name = "purnima";
+
+$anon = function(){
+	echo $message;
+};
+$anon();
+
+$anon2 = function($arg) use($message,$name){
+	echo $arg ." ".$name;
+};
+$anon2("hello");
+echo $message;
